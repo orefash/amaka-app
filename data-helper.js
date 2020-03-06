144118b96ade0b94b1d0f52e368af216e66c2d52
Glitch (hello-sqlite) <none>
2020-03-06 16:10:50 +0000

    🕍🎤 Checkpoint ./data-helper.js:9810357/732

--- data-helper.js
+++ data-helper.js
@@ -19,12 +19,22 @@ module.exports = {
   },
   add_student: function(data) {
     
-    var query_vals = `("${oid}", "${oid}", "${fname}", "${lname}", "${date}", "${email}", "${address}", "${phone}", "${user_id}")`;
+    var query_vals = `("${data.s_id}", "${data.fname}", "${data.lname}", "${data.dob}", "${data.dob}", "${data.address}", "${data.gender}", "${data.bl_grp}", "${data.bl_typ}", "${data.class}", "${data.prior_health}", "${data.prior_med}", ${data.weight}, ${data.height}, "${data.date}")`;
   var query =
-    "INSERT INTO students (s_id, fname, lname, dob, address, gender, bl_grp, bl_typ, class, prior_health, prior_med, weight) VALUES " +
+    "INSERT INTO students (s_id, fname, lname, dob, address, gender, bl_grp, bl_typ, class, prior_health, prior_med, weight, height, date) VALUES " +
     query_vals;
   db.serialize(() => {
-    db.run(query);
+    db.run(query, function(err) {
+    if (err) {
+      console.log(err.message);
+      return 1;
+    }else{
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+      return 0;
+    }
+    // get the last insert id
+    
+    });
   });
 
     
