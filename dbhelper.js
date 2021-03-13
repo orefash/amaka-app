f37b8e019a2feeb9b6573571e641ace5a728f5f3
Glitch (cuddly-dusty-psychiatrist) <none>
2021-03-13 23:13:54 +0100

    packsge and db update

--- dbhelper.js
+++ dbhelper.js
@@ -1,7 +1,7 @@
 // init sqlite db
 
 const fs = require("fs");
-const dbFile = "./.data/sqlite.db";
+const dbFile = ".data/sqlite.db";
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
